930eb3e0163a35558488421f8488c90c6ca2ee1b
Nieninque <mail.de.rose@gmail.com>
2017-03-27 10:29:20 +0200

    modifs des garçons

--- src/db/ConnectDB.java
+++ src/db/ConnectDB.java
@@ -21,8 +21,8 @@ public class ConnectDB {
     // Assign values ​​for connection db mysql
     private static final String DB_URL = "jdbc:mysql://localhost:3306/airafpa";
     private static final String DB_JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DB_USER = "afpa";
-    private static final String DB_PASSWORD = "afpa";
+    private static final String DB_USER = "formation";
+    private static final String DB_PASSWORD = "afpagen";
 
     private Connection cn = null;
 
